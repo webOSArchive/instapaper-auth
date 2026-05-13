2c2442a733af5f88d033979692205166a28eb20b
codepoet80 <codepoet80@github.com>
2026-05-13 18:48:08 -0400

    add margin

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include($docRoot . "config.php");
 include($docRoot . "common.php");
 echo file_get_contents("https://www.webosarchive.org/app-template/header.php?docRoot=" . urlencode($docRoot) . "&appTitle=" . urlencode($appTitle) . "&protocol=" . findProtocol());
 ?>
-<p align="middle">Instapaper is available on webOS! You'll need an app for your device to get started.</p>
+<p align="middle" style="margin-top:18px">Instapaper is available on webOS! You'll need an app for your device to get started.</p>
 <p align="middle">Check out <a href="<?php echo findProtocol(); ?>://appcatalog.webosarchive.org/app/ReadOnTouchPRO3.0">ReadOnTouch Pro</a> in the App Museum!</p>
 <br/>
 <p align="middle">Got the app and ready to <a href="/activate">activate</a>?
